e1d5379b92a162fbab29de0b3634816cfed9ff39
richareward360 <richa.agarwal@reward360.co>
2019-11-21 10:25:17 +0530

    Branch1- file1

---
+++ app/src/main/java/com/example/tryrebase/Branch1File1Activity.java
@@ -0,0 +1,14 @@
+package com.example.tryrebase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Branch1File1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_branch1_file1);
+    }
+}
